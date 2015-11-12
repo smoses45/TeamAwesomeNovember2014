1ffcdda4d867dd29491025ff1705b5326cd3664e
rabidang3ls <heyabbottb@gmail.com>
2015-11-11 21:02:48 -0700

    Update portspoof.sh
    Added comments. iptables lines must be added before running this sciprt.

--- Linux/portspoof.sh
+++ Linux/portspoof.sh
@@ -6,7 +6,12 @@ cd portspoof/
 sudo ./configure --sysconfdir=/etc/
 sudo make
 sudo make install
-sudo iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 0:1775 -j REDIRECT --to-ports 4444
-sudo iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 1777:65535 -j REDIRECT --to-ports 4444
+
+#Every port that needs to be open must be excluded from the redirect rules.
+#The example lines below leave ports 22, 23, and 80 open and redirect all other traffic to portspoof
+#sudo iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 1:21 -j REDIRECT --to-ports 4444
+#sudo iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 24:79 -j REDIRECT --to-ports 4444
+#sudo iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 81:65535 -j REDIRECT --to-ports 4444
+
 sudo portspoof -c /etc/portspoof.conf -s /etc/portspoof_signatures -D
 ps -ef | grep spoof
